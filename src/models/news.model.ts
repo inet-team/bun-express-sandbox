9eff0a8124e3acab3c8809838b4eeb1bd2ce7fe2
989x <99growsup@gmail.com>
2025-06-04 00:51:20 +0700

    chore(news): update category slug from 'healthcare' to 'medical'

--- src/models/news.model.ts
+++ src/models/news.model.ts
@@ -11,7 +11,7 @@ export interface NewsThumbnail {
 // Fixed list of categories with known id-name mapping
 export type NewsCategory =
   | { id: 'demo66c11b01'; slug: 'sustainability'; name: 'ความยั่งยืน' }
-  | { id: 'demo66c11b02'; slug: 'healthcare'; name: 'การแพทย์' }
+  | { id: 'demo66c11b02'; slug: 'medical'; name: 'การแพทย์' }
   | { id: 'demo66c11b03'; slug: 'economy-investment'; name: 'เศรษฐกิจ - การลงทุน' }
   | { id: 'demo66c11b04'; slug: 'social'; name: 'สังคม' }
   | { id: 'demo66c11b05'; slug: 'it-business'; name: 'ธุรกิจไอที' };
